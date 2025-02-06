ffdb708abf771a501cfa7a8e304d3336bc104cd6
alexandrecldias <alexandrecldias@gmail.com>
2025-02-06 20:54:01 -0300

    adicionado a resposta da primeira questao

--- Questao1/ContaBancaria.cs
+++ Questao1/ContaBancaria.cs
@@ -2,8 +2,39 @@
 
 namespace Questao1
 {
-    class ContaBancaria {
+    class ContaBancaria
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        private double Saldo;
 
-       
+        private const double TaxaSaque = 3.50;
+
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = 0.0;
+        }
+
+        public ContaBancaria(int numero, string titular, double depositoInicial) : this(numero, titular)
+        {
+            Deposito(depositoInicial);
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= (quantia + TaxaSaque);
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {Numero}, Titular: {Titular}, Saldo: $ {Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
     }
 }
