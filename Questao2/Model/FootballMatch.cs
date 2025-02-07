8197b380c0f33b6c7f02d0c3e68f2793b83460b2
alexandrecldias <alexandrecldias@gmail.com>
2025-02-06 22:38:04 -0300

    resolucao da questao 2

---
+++ Questao2/Model/FootballMatch.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao2.Model
+{
+    public class FootballMatch
+    {
+        [JsonProperty("competition")]
+        public string Competition { get; set; }
+
+        [JsonProperty("year")]
+        public int Year { get; set; }
+
+        [JsonProperty("round")]
+        public string Round { get; set; }
+
+        [JsonProperty("team1")]
+        public string Team1 { get; set; }
+
+        [JsonProperty("team2")]
+        public string Team2 { get; set; }
+
+        [JsonProperty("team1goals")]
+        public string Team1Goals { get; set; }
+
+        [JsonProperty("team2goals")]
+        public string Team2Goals { get; set; }
+    }
+}
